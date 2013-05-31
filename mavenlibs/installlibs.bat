f362734da0da5531cf85ed0c4db028e17d746640
Sandile Keswa <sandile.keswa@gmail.com>
2013-05-31 12:44:25 -0400

    Fixed build.

---
+++ mavenlibs/installlibs.bat
@@ -0,0 +1,6 @@
+call mvn install:install-file -Dfile=jic-framework-2.0.0.jar -DgroupId=de.fraunhofer.fokus -DartifactId=jic-framework -Dversion=2.0.1 -Dpackaging=jar
+call mvn install:install-file -Dfile=bpmnq-1.0.3-SNAPSHOT.jar -DgroupId=com.bpmnq -DartifactId=bpmnq -Dversion=1.0.3-SNAPSHOT -Dpackaging=jar
+call mvn install:install-file -Dfile=SuperCSV-1.52.jar -DgroupId=org.supercsv -DartifactId=SuperCSV -Dversion=1.52 -Dpackaging=jar
+call mvn install:install-file -Dfile=jbpt-0.1.0.jar -DgroupId=jbpt -DartifactId=jbpt -Dversion=0.1.0 -Dpackaging=jar
+call mvn install:install-file -Dfile=oryxAtlas-1.0.0.jar -DgroupId=de.hpi.bpt -DartifactId=oryxAtlas -Dversion=1.0.0 -Dpackaging=jar
+call mvn install:install-file -Dfile=jta-1.0.1B.jar -DgroupId=javax.transaction -DartifactId=jta -Dversion=1.0.1B -Dpackaging=jar
